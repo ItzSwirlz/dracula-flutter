1f4b13e06317585edcd6ba87b9f404702b328d33
Joshua Peisach <itzswirlz2020@outlook.com>
2022-03-17 02:11:29 -0400

    Import color palette

---
+++ lib/colors.dart
@@ -0,0 +1,37 @@
+library dracula_theme;
+
+import 'package:flutter/material.dart';
+
+/// See https://spec.draculatheme.com/
+class DraculaColors {
+  DraculaColors._();
+
+  static const Color background = Color(0xFF282A36);
+  static const Color foreground = Color(0xFFF8F8F2);
+  static const Color selection = Color(0xFF44475A);
+
+  static const Color red = Color(0xFFFF5555);
+  static const Color orange = Color(0xFFFFB86C);
+  static const Color yellow = Color(0xFFF1FA8C);
+  static const Color green = Color(0xFF50FA7B);
+  static const Color purple = Color(0xFFBD93F9);
+  static const Color cyan = Color(0xFF8BE9FD);
+  static const Color pink = Color(0xFFFF79C6);
+
+  static const Color ansiBlack = Color(0xFF21222C);
+  static const Color ansiRed = Color(0xFFFF5555);
+  static const Color ansiGreen = Color(0xFF50FA7B);
+  static const Color ansiYellow = Color(0xFFF1FA8C);
+  static const Color ansiBlue = Color(0xFFBD93F9);
+  static const Color ansiMagenta = Color(0xFFFF79C6);
+  static const Color ansiCyan = Color(0xFF8BE9FD);
+  static const Color ansiWhite = Color(0xFFF8F8F2);
+  static const Color ansiBrightBlack = Color(0xFF6272A4);
+  static const Color ansiBrightRed = Color(0xFFFF6E6E);
+  static const Color ansiBrightGreen = Color(0xFF69FF94);
+  static const Color ansiBrightYellow = Color(0xFFFFFFA5);
+  static const Color ansiBrightBlue = Color(0xFFD6ACFF);
+  static const Color ansiBrightMagenta = Color(0xFFFF92DF);
+  static const Color ansiBrightCyan = Color(0xFFA4FFFF);
+  static const Color ansiBrightWhite = Color(0xFFFFFFFF);
+}
---
+++ lib/dracula.dart
@@ -0,0 +1,3 @@
+library dracula_theme;
+
+export 'package:dracula_theme/colors.dart';
